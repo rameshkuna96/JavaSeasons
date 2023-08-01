147bbacb2cf97cb763d2fe3aad08345814de7b72
rameshkuna96 <rameshkuna96@gmail.com>
2023-08-01 15:59:38 +0530

    first commit

---
+++ src/july1st/ChromeLaunch.java
@@ -0,0 +1,16 @@
+package july1st;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeLaunch {
+
+	public static void main(String[] args) {
+		
+		//System.setProperty("webdriver.chrome.driver", "D:\\K.Ramesh\\Selenium class\\Softwares\\chrome-win32\\chrome-win32\\chromedriver.exe");
+
+		
+		new ChromeDriver();
+
+	}
+
+}
